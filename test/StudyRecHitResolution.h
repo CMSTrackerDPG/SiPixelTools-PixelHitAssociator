769594d94520b6552da82b2f851e327c1d8294ff
Dinko Ferencek <Dinko.Ferencek@cern.ch>
2020-11-05 16:01:33 +0100

    updated header file path

--- test/StudyRecHitResolution.h
+++ test/StudyRecHitResolution.h
@@ -58,7 +58,7 @@
 //#define PIXEL_ASSOCIATOR // use my special pixel associator
 
 #ifdef PIXEL_ASSOCIATOR
-#include "DPGAnalysis-SiPixelTools/PixelHitAssociator/interface/PixelHitAssociator.h"
+#include "SiPixelTools/PixelHitAssociator/interface/PixelHitAssociator.h"
 #else 
 #include "SimTracker/TrackerHitAssociation/interface/TrackerHitAssociator.h"
 #endif
